5309e72decbb935384eef4a4587ab3a8035c529d
Hasan Kc <alhasanal_qaysi@yahoo.com>
2022-09-29 18:59:45 +0300

    Done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,32 +8,16 @@ const Button = ({ handleClick, text }) => {
   )
 }
 
-
-
 const Display = props => <div>{props.text} {props.value} </div>
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [allClicks, setAll] = useState(0)
-  const [selected, setSelected] = useState(0)
 
-  const getRandom = () => {
-    const selected = Math.floor(Math.random() * anecdotes.length)
-    setSelected(selected)
-    
-  }
 
   const handleClickGood = () => {
     setAll(allClicks + 1)
@@ -48,8 +32,10 @@ const App = () => {
     setBad(bad + 1)
   }
   const Percentage = () => {
+    const total = good + neutral + bad
+
     if (allClicks > 0) {
-      return ((good + bad + neutral) / 100) * 100
+      return (good * (100 / total))
     }
   }
   const getAverage = () => {
@@ -63,8 +49,7 @@ const App = () => {
     return (
       <div>
         <h1>Give feddback</h1>
-        <Button handleClick={getRandom} text= 'generate'/>
-        <p>{anecdotes[selected]}</p>
+
         <Button handleClick={handleClickGood} text='good' />
         <Button handleClick={handleClickNeutral} text='neutral' />
         <Button handleClick={handleClickBad} text='bad' />
